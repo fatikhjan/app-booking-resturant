518d4109995b0657806adabc44e594df355d5a39
Fotikhjon <muhammad2004fotih@gmail.com>
2023-05-20 11:29:33 +0500

    First Commit without anything

---
+++ app-booking-resturant/src/main/java/uz/momentuminnovations/appbookingresturant/AppBookingResturantApplication.java
@@ -0,0 +1,13 @@
+package uz.momentuminnovations.appbookingresturant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppBookingResturantApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppBookingResturantApplication.class, args);
+    }
+
+}
---
+++ app-booking-resturant/src/test/java/uz/momentuminnovations/appbookingresturant/AppBookingResturantApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.momentuminnovations.appbookingresturant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppBookingResturantApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
